5d1c5ff4dcbd3f1f430694c5e97f0f3a1ed035ec
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-14 15:33:28 -0700

    added controls tests for render, and for buttons firing toggle functions on click for close and lock

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,6 +3,37 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Controls from "./Controls";
 
+test("Controls render without error", () => {
+    render(<Controls />);
+});
+
+test("provides buttons to toggle closed states", () => {
+    const toggleClosedMock = jest.fn();
+
+    const { getByText } = render(
+        <Controls
+            locked={false}
+            closed={false}
+            toggleClosed={toggleClosedMock}
+        />);
+    fireEvent.click(getByText(/close/i));
+    expect(toggleClosedMock).toHaveBeenCalled();
+
+});
+
+test("provides buttons to toggle locked state", () => {
+
+    const toggleLockedMock = jest.fn();
+    const { getByText } = render(
+        <Controls
+            locked={false}
+            closed={true}
+            toggleLocked={toggleLockedMock}
+        />);
+    fireEvent.click(getByText(/lock/i));
+    expect(toggleLockedMock).toHaveBeenCalled();
+});
+
 test("Gate cannot be opened if locked", () => {
 
     const toggleClosedMock = jest.fn();
