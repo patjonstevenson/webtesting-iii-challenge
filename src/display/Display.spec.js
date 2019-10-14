62bd87803eb4fa1055e483314085e16a52402d79
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-14 15:11:37 -0700

    tests display for closed/open if closed true/false

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -7,3 +7,13 @@ import Display from "./Display";
 test("Display renders without error", () => {
     render(<Display />);
 });
+
+test("Displays closed if closed is true", () => {
+    const { getByText } = render(<Display closed={true} />);
+    getByText(/closed/i);
+});
+
+test("Displays open if closed is false", () => {
+    const { getByText } = render(<Display closed={false} />);
+    getByText(/open/i);
+});
