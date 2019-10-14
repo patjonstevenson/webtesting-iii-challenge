c6263bf846ae5f7ca7dd27ce8d46ebca6938ac63
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-14 14:19:04 -0700

    added test for controls (need to move it from dashboard to controls test file)

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,36 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+import Controls from "../controls/Controls";
+import Display from "../display/Display";
+
+test("Gate defaults to unlocked and open", () => {
+    const { getByText, queryByText } = render(<Dashboard />);
+    getByText(/open/i);
+    expect(queryByText(/open gate/i)).toBeNull();
+    getByText(/unlocked/i);
+    expect(queryByText(/unlock gate/i)).toBeNull();
+})
+
+test("Gate cannot be opened if locked", () => {
+
+    const toggleClosedMock = jest.fn();
+    const { getByText } = render(<Controls locked={true} closed={true} toggleClosed={toggleClosedMock} />);
+
+    fireEvent.click(getByText(/open/i));
+    expect(toggleClosedMock).toHaveBeenCalledTimes(0);
+})
+
+test("Gate cannot be locked if open", () => {
+    const toggleLockedMock = jest.fn();
+
+    const { getByText } = render(<Controls locked={false} closed={false} toggleLocked={toggleLockedMock} />);
+
+    fireEvent.click(getByText(/lock/i));
+    expect(toggleLockedMock).toHaveBeenCalledTimes(0);
+})
+
+test("Shows the controls and display", () => {
+
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+
+test("Display renders without error", () => {
+    render(<Display />);
+});
